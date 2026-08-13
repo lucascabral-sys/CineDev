09bcd72ff045412599d6e58181c2885a58a120cb
lucascabral-sys <lucas.patalo.cabral@escola.pr.gov.br>
2026-08-13 09:19:11 -0300

    asd

--- Catalogo.js
+++ Catalogo.js
@@ -1,7 +1,8 @@
-export default class Catalogo {
-    #listaDeMidias;
-    constructor() {
-        this.#listaDeMidias = [];
+export default class Catalogo { // cria a classe Catalogo 
+    #listaDeMidias; // declara uma propriedade
+    constructor() { // é um metodo especial  da classe
+        this.#listaDeMidias = []; // this é um objeto atual
+        // criar um array vazio
     }
     adicionar(midia) {
         this.#listaDeMidias.push(midia);
@@ -19,4 +20,5 @@ export default class Catalogo {
         return catalogoInstanciado.#listaDeMidias.filter(
         m => m.genero.toLowerCase() === generoAlvo.toLowerCase()
     }
+}
 }
